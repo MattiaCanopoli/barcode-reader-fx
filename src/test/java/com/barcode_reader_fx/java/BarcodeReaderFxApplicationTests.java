8e6f7c30ce0c5dc486a73a9372c79f51f10d0b14
MattiaCanopoli <canopoli.mattia@gmail.com>
2025-01-11 17:32:25 +0100

    starting project

---
+++ src/test/java/com/barcode_reader_fx/java/BarcodeReaderFxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barcode_reader_fx.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarcodeReaderFxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
